e6506fe99525f3c81262c4834d2c5af5cfb3ff23
CesarBustos12 <cesarbustos897@gmail.com>
2024-09-15 15:27:33 -0600

    Refactoriza el boton para que sea re utilizable

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
 
-function  Button ()  {
+function Button({
+  title = "default",
+  color = "btn-primary",
+  onClick = () => {},
+}) {
   return (
-    <button class="cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg
-border-blue-600
-border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
-active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">
-  Boton
-</button>
-  )
+    <button onClick={onClick} className={`btn ${color}`}>
+      {title}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
