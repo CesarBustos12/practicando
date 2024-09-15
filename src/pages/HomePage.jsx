b47d9e1a4d53a34649e966e04e86db4b661c282c
CesarBustos12 <cesarbustos897@gmail.com>
2024-09-15 15:40:17 -0600

    Refactorizamos la pagina principal de mi sitio

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../sections/Navbar';
+
+function HomePage  ()  {
+  return (
+    <div>
+      <Navbar />
+
+    </div>
+  )
+}
+
+export default HomePage;
