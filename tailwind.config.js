76c757786ed0fd6874f02bdabfee143f79d2b8b6
CesarBustos12 <cesarbustos897@gmail.com>
2024-09-20 14:10:12 -0600

    Agrega la paleta de colores a utilizar para los componentes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-  ],
-}
+  plugins: [require("daisyui")],
+  daysiui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#1E40AF",
+          secondary: "#9333EA",
+          accent: "#FBBF24",
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+};
