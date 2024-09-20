ba47874fa6226a2e0e78a71459551d88e27beee2
CesarBustos12 <cesarbustos897@gmail.com>
2024-09-20 14:11:12 -0600

    Agrega mas rutas para una mejor navegacion

--- src/routers/Routes.jsx
+++ src/routers/Routes.jsx
@@ -1,13 +1,20 @@
 import MinimalLayout from "../layouts/MinimalLayout";
-import { useRoutes } from "react-router-dom";
 import React, { Children } from "react";
 import HomePage from "../pages/HomePage";
+import Nosotros from "../sections/Nosotros";
+import Contacto from "../sections/Contacto";
+import Login from "../sections/Login";
+import UploadFile from "../sections/UploadFile";
 
 const Routes = {
     path: "/",
     element: <MinimalLayout />,
     children: [
         {path: "/", element: < HomePage/>},
+        {path: "/nosotros", element: <Nosotros />},
+        {path: "/contacto", element: <Contacto />},
+        {path: "/login", element: <Login />},
+        {path: "/subir", element: <UploadFile />}
     ]
 };
 
