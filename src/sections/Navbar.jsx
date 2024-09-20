a27112708b904f426db1a0abcba914e626a77812
CesarBustos12 <cesarbustos897@gmail.com>
2024-09-20 14:14:29 -0600

    Mejora la navegacion entre los componentes

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
  function Navbar  ()  {
   return (
    
-   <div className="navbar bg-blue-700 border-2 border-black rounded-lg shadow-xl">
+   <div className="navbar bg-primary border-black rounded-lg">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">Pruebas Site</a>
+    <Link className="btn btn-ghost text-xl" to={"/"}>Page</Link>
   </div>
   <div className="flex-none">
     <ul className="menu menu-horizontal px-1">
-      <li><a className='menu-link' href='/'>Inicio</a></li>
-      <li><a className='menu-link' href='/nosotros'>Nosotros</a></li>
-      <li><a className='menu-link' href='/contacto'>Contacto</a></li>
+      <li><Link className='menu-link' to='/'>Inicio</Link></li>
+      <li><Link className='menu-link' to='/nosotros'>Nosotros</Link></li>
+      <li><Link className='menu-link' to='/contacto'>Contacto</Link></li>
       <li>
         <details>
           <summary>Menu</summary>
-          <ul className="bg-blue-700 border-2 border-black border-inherit px-1">
-            <li><a className='menu-link' href='/login'>Iniciar sesion</a></li>
-            <li><a className='menu-link' href='/subir'>Subir archivos</a></li>
+          <ul className="bg-primary">
+            <Link className='menu-link' to='/subir'>Subir Archivos</Link>
           </ul>
         </details>
       </li>
