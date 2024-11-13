a90b403e29e16a7db48d28a0c2fbc9f6330e155a
CesarBustos12 <cesarbustos897@gmail.com>
2024-11-12 22:44:38 -0600

    feat: agrega una nueva barra de navegacion

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -3,27 +3,46 @@ import { Link } from 'react-router-dom';
 
  function Navbar  ()  {
   return (
-   
-   <div className="navbar bg-primary border-black rounded-lg">
-  <div className="flex-1">
-    <Link className="btn btn-ghost text-xl" to={"/"}>Page</Link>
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-      <li><Link className='menu-link' to='/'>Inicio</Link></li>
-      <li><Link className='menu-link' to='/nosotros'>Nosotros</Link></li>
-      <li><Link className='menu-link' to='/contacto'>Contacto</Link></li>
-      <li>
-        <details>
-          <summary>Menu</summary>
-          <ul className="bg-primary">
-            <Link className='menu-link' to='/subir'>Subir Archivos</Link>
-          </ul>
-        </details>
-      </li>
-    </ul>
-  </div>
-</div>
+    <nav className="fixed top-0 left-0 w-full bg-transparent backdrop-blur-md shadow-md z-10">
+    <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+      <div className="text-white text-lg font-semibold">
+        <a href="#">Pinetech</a>
+      </div>
+      <ul className="flex space-x-8 text-sm text-white font-medium">
+        <li>
+          <a href="#" className="hover:text-gray-300">
+            Productos
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-gray-300">
+            Soluciones
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-gray-300">
+            Recursos
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-gray-300">
+            Codigo Abierto
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-gray-300">
+            Acerca de
+          </a>
+        </li>
+      </ul>
+      <div className="flex items-center space-x-4">
+        <button className="text-white hover:text-gray-300">Ingresar</button>
+        <button className="text-white border border-white px-4 py-1 rounded hover:bg-gray-700 transition duration-200">
+          Registrarse
+        </button>
+      </div>
+    </div>
+  </nav>
   );
 }
 
