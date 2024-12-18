6de5e906293de33dfc924fa50b627f9a0c18d6bb
CesarBustos12 <cesarbustos897@gmail.com>
2024-12-18 12:47:32 -0600

    feat: añade nuevas rutas

--- src/routers/Routes.jsx
+++ src/routers/Routes.jsx
@@ -1,18 +1,25 @@
+//Importaciones necesarias para nuestra pagina web
 import MinimalLayout from "../layouts/MinimalLayout";
 import React, { Children } from "react";
 import Nosotros from "../sections/Nosotros";
 import Contacto from "../sections/Contacto";
 import Login from "../sections/Login";
 import { LandingPage } from "../pages/LandingPage";
+import { DesingPage } from "../pages/DesingPage";
+import HomePage from "../pages/HomePage";
+import { PaginaDiseño } from "../pages/PaginaDiseño";
 
 const Routes = {
     path: "/",
     element: <MinimalLayout />,
     children: [
-        {path: "/", element: < LandingPage/>},
+        {path: "/", element: < HomePage/>},
+        {path: "/landingpage", element: <LandingPage/>},
         {path: "/nosotros", element: <Nosotros />},
         {path: "/contacto", element: <Contacto />},
         {path: "/login", element: <Login />},
+        {path: "/desing", element: <DesingPage />},
+        {path: "/pagina", element: <PaginaDiseño />},
     ]
 };
 
