c54b086a4a5983adac508f0c8456ba438ae65708
CesarBustos12 <cesarbustos897@gmail.com>
2024-12-18 12:46:31 -0600

    feat: cambia el componente base del boton

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,16 @@
-function Button ({
-  title = "Default",
-  classNames = "",
-  type = "button",
-  onClick = () => {},
-}) {
-  return (
-    <button
-      onClick={(e) => onClick(e)}
-      type={type}
-      className={`bg-inherit text-black px-6 py-2 rounded-lg ${classNames}`}
-    >
-      {title}
-    </button>
-  );
-}
+import React from 'react';
+
+const Button = ({ 
+  Titulo = "default", 
+  type = "submit",
+  color = "btn-primary",
+  className = "",
+  onClick = () => {},    
+ }) => (
+  <button type={type} onClick={onClick} className={`btn ${className}`}>
+    {Titulo}
+  </button>
+);
 
 export default Button;
+
